a81cda360de39851efa8a7e96e61483b79380819
TodorokiDeku <51161901+Jovantri10@users.noreply.github.com>
2020-10-07 22:38:01 +0700

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://enshrined-better-word.glitch.me"]
+const urls = ["https://official-giveaway-bot.newt10.repl.co"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
