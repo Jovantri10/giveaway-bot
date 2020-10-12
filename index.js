d9bd421705b52fbfc830dfdb2f32f9a1210543a4
TodorokiDeku <51161901+Jovantri10@users.noreply.github.com>
2020-10-12 19:01:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://official-giveaway-bot.newt10.repl.co"]
+const urls = ["https://wirehaired-sheer-wavelength.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
